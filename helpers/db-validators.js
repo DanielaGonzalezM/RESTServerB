6d6ff0aecf1330d927a8e35ec5b6a427dcaf596d
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2023-09-19 17:51:50 -0300

    Sección 9: Alcances del RESTServer y mantenimiento

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -2,30 +2,30 @@ const Role = require("../models/role");
 const Usuario = require("../models/usuario");
 
 const isValidRole = async (rol = "") => {
-    const existeRol = await Role.findOne({ rol });
-    if (!existeRol) {
-        throw new Error(`El rol ${rol} no existe`);
-    }
+  const existeRol = await Role.findOne({ rol });
+  if (!existeRol) {
+    throw new Error(`El rol ${rol} no existe`);
+  }
 };
 
 const emailExists = async (correo = "") => {
-    const existeEmail = await Usuario.findOne({ correo });
-    if (existeEmail) {
-        throw new Error(`El correo ${correo} ya está registrado`);
-    }
+  const existeEmail = await Usuario.findOne({ correo });
+  if (existeEmail) {
+    throw new Error(`El correo ${correo} ya está registrado`);
+  }
 };
 
 const idExists = async (id = "") => {
-    const existeUsuario = await Usuario.findById(id);
-    console.log(existeUsuario);
+  const existeUsuario = await Usuario.findById(id);
+  console.log(existeUsuario);
 
-    if (!existeUsuario) {
-        throw new Error(`El usuario ${id} no existe`);
-    }
+  if (!existeUsuario) {
+    throw new Error(`El usuario ${id} no existe`);
+  }
 };
 
 module.exports = {
-    isValidRole,
-    emailExists,
-    idExists
+  isValidRole,
+  emailExists,
+  idExists,
 };
