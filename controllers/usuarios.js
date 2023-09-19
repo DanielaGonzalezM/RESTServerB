cca7506b964012964cded04695d9deb11466a300
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2023-09-19 17:20:35 -0300

    Sección 9: Alcances del RESTServer y mantenimiento

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
 const { response, request } = require("express");
 const bcryptjs = require("bcryptjs");
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
   const { q, nombre, apikey } = req.query;
@@ -11,12 +12,19 @@ const usuariosGet = (req = request, res = response) => {
   });
 };
 
-const usuariosPut = (req = request, res = response) => {
+const usuariosPut = async (req = request, res = response) => {
   const { id } = req.params;
-
+  const { password, google,correo, ...resto } = req.body;
+  //TODO validar contraseña base de datos
+  if (password) {
+    //Encriptar password
+    const salt = bcryptjs.genSaltSync(10);
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
   res.status(500).json({
     msg: "put API - controlador",
-    id,
+    usuario,
   });
 };
 
@@ -24,7 +32,7 @@ const usuariosPost = async (req = request, res = response) => {
   const { nombre, correo, password, rol } = req.body;
   const usuario = new Usuario({ nombre, correo, password, rol });
 
-  //Encruptar password
+  //Encriptar password
   const salt = bcryptjs.genSaltSync(10);
   usuario.password = bcryptjs.hashSync(password, salt);
   //Guardar en BD
